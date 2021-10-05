0468ffe5428fbf3032570e3c8b12c4ab7087d8f3
Pitney Jules <PJules602@gmail.com>
2021-10-05 15:29:36 -0400

    House Function Added

---
+++ House.cs
@@ -0,0 +1,7 @@
+namespace HouseFunction{ 
+
+public class House {
+
+}
+
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+// This is a comment
+
+/*
+
+Will be a comment
+- We use PascelCase for the most part and that means namnig artifacts as EverythingWillBeCapitalized
+- We use CamelCase for naming fields and that emans naming artifacts as onlyOnceYouHaveALowerCaseWordAtTheBeginning
+
+*/ 
 
 namespace HelloWorld
 {
     class Program
     {
+
+        /*
+        - Main method is the first method that will be called to run
+        - Static means I don't have to instantiate the program class to use that method
+        - Void will not return any value    
+        */
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Program.Example(); 
+            // Program test = new Program(); //This will instantiate an object. (Only necessary if the method is not static)
+            test.Example()
+        }
+
+        public static int Example(){
+
+            return 10; 
         }
     }
 }
